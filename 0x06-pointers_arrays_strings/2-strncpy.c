33c7022ba936036730d2acf91e8b8e199c3cb76f
faykey <77450186+faykey@users.noreply.github.com>
2022-09-22 22:17:01 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,36 @@
-#include "main.h"
-#include <string.h>
-
 /**
-* _strncpy - copies a string of bytes from
-*            src into dest.
-* @dest: stores string copy.
-* @src: source string.
-* @n: The maximum number of character to copy from src to dest
-*
-* Return: A pointer to the resulting string dest.
+ * _strncpy - A function that copies a string.
+ *
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
+ *
+ * Return: @dest
 */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	while (dest[n])
+	int i;
+
+	/**
+	 * iterate through src array
+	 * where if there is no null byte
+	 * among the first n bytes of source
+	 * the string placed in dest will not be
+	 * null terminated
+	*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	/**
+	 * if the length of source is less than n
+	 * write additional nullbytes to dest to
+	 * ensure that a total of n bytes is written
+	*/
+	while (i < n)
 	{
-		n++;
+		dest[i] = '\0';
+		i++;
 	}
-	strcpy(dest, src);
 
 	return (dest);
 }
