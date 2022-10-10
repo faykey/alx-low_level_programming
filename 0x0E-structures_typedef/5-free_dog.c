061c4b7bfd14802186ca390e155fbf5c9b6edf2a
faykey <77450186+faykey@users.noreply.github.com>
2022-10-10 22:20:19 +0100

    Create 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that frees a dog
+ * @d:pointer
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
