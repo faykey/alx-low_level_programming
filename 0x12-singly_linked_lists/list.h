d304e689443a6c1140ff83199b2963ca7da7194e
faykey <77450186+faykey@users.noreply.github.com>
2022-10-21 05:58:16 +0100

    Update list.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,13 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 /**
- * struct list_s - node list struct
- * @str: string
- * @len: length
- * @next: next struct pointer
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for ALX project
  */
 typedef struct list_s
 {
@@ -15,15 +15,11 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-typedef struct node
-{
-	char *str;
-	struct node *next;
-} node;
 
-int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
-#endif
+void free_list(list_t *head);
+int _strlen(const char *s);
+#endif /* #ifndef LISTS_H */
