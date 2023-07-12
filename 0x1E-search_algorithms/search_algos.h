7cf5fd4fb5a4b99c7acc39a150a13f12f9d4073e
faykey <fikayomicheal4@gmail.com>
2023-07-12 14:13:44 -0700

    betty adjustments

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -20,20 +20,24 @@ typedef struct listint_s
 } listint_t;
 
 /**
- * struct listint_s - singly linked list
+ * struct skiplist_s - Singly linked list with an express lane
  *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
  *
- * Description: singly linked list node structure
+ * Description: singly linked list node structure with an express lane
  */
-typedef struct listint_s
+typedef struct skiplist_s
 {
     int n;
     size_t index;
-    struct listint_s *next;
-} listint_t;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+
 
 
 int linear_search(int *array, size_t size, int value);
@@ -44,3 +48,5 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+
+#endif
